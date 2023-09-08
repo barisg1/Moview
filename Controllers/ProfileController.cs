15ac81563a0a71bcac831be58b0b69273483e94f
Özlem KILIÇ <122043812+OzlemKlc@users.noreply.github.com>
2023-09-08 10:37:38 +0300

    profile page

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moview.Models;
+
+namespace Moview.Controllers
+{
+
+    public class ProfileController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProfileController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Profil bilgilerini görüntülemek için action metodu
+        public IActionResult Index()
+        {
+            // Kullanıcıyı belirlemek için kimlik doğrulama işlemleri
+            // Örnek olarak: var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var userProfile = _context.Users.FirstOrDefault(); // Veritabanından kullanıcı bilgilerini çekin
+            return View(userProfile);
+        }
+
+        // Profil bilgilerini düzenlemek için action metodu
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            // Kullanıcıyı belirlemek için kimlik doğrulama işlemleri
+            // Örnek olarak: var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var userProfile = _context.Users.FirstOrDefault(); // Veritabanından kullanıcı bilgilerini çekin
+            return View(userProfile);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Kullanıcıyı belirlemek için kimlik doğrulama işlemleri
+                // Örnek olarak: var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                // Kullanıcı profil bilgilerini güncelleme işlemleri
+                var existingUser = _context.Users.FirstOrDefault(); // Veritabanından kullanıcı bilgilerini çekin
+                if (existingUser != null)
+                {
+                    existingUser.FirstName = user.FirstName;
+                    existingUser.LastName = user.LastName;
+                    existingUser.Email = user.Email;
+
+                    _context.SaveChanges(); // Veritabanındaki değişiklikleri kaydedin
+                }
+                return RedirectToAction("Index"); // Başarıyla güncelleme yapıldıktan sonra yönlendirme
+            }
+            return View(user);
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Moview.Models
+{
+    public class Users
+    {
+
+        [Key]
+        public int UserID { get; set; }
+
+
+            
+            public string Username { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        
+            public string Email { get; set; }
+         
+            public string Password { get; set; }
+           
+
+
+      
+        }
+    }
+
